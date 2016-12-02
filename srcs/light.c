e57d69bae364f69ed58c857ecc6d81f2d132b87e
Pierre MARTINEAU <pmartine@e1r6p14.42.fr>
2016-12-02 19:37:18 +0100

    light.c : ombres + specular a faire

--- srcs/light.c
+++ srcs/light.c
@@ -6,7 +6,7 @@
 /*   By: pmartine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 17:16:04 by pmartine          #+#    #+#             */
-/*   Updated: 2016/12/02 17:45:22 by pmartine         ###   ########.fr       */
+/*   Updated: 2016/12/02 18:40:03 by pmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,12 @@ double      min_max(double numb, double min, double max)
 	t_vec	ref;
 
 	spec = 0.0;
-	ref = ft_vectorscale(&mlx->norm, (2.0 * ft_vectordot(&mlx->norm, &dist)));
-	ref = ft_vectorsub(&dist, &ref);
-	if ((tmp = ft_vectordot(&ref, &mlx->ray_dir)) > 0.0)
+	ref = scale_vect(&mlx->norm, (2.0 * ft_vectordot(env->norm, dist)));
+	ref = sub_vect(dist, ref);
+	if ((tmp = dot_vect(ref, env->ray_dir)) > 0.0)
 	{
 		spec = pow(tmp, 30.0) * 1.0;
-		spec = ft_clamp(spec, 0.0, 1.0);
+		spec = min_max(spec, 0.0, 1.0);
 	}
 	return (spec);
 }
@@ -50,7 +50,7 @@ double   calc_lamb(t_env *env, t_obj *obj)
 	dist = sub_vect(env->spots->pos, cam);
 	dist = norm_vect(dist);
 	lambert = 0;
-	lambert += dot_vect(dist, norm);
+	lambert += dot_vect(dist, norm) * 0.9;
 	lambert = min_max(lambert, 0.15, 1.0) ;
 	return (lambert);
 }
