6f24a7cd42694f3340b5a592439304e52e6ead26
Pierre MARTINEAU <pmartine@e1r6p14.42.fr>
2016-12-05 12:49:03 +0100

    specular

--- srcs/light.c
+++ srcs/light.c
@@ -6,7 +6,7 @@
 /*   By: pmartine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 17:16:04 by pmartine          #+#    #+#             */
-/*   Updated: 2016/12/02 18:40:03 by pmartine         ###   ########.fr       */
+/*   Updated: 2016/12/05 12:48:11 by pmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,36 +21,29 @@ double      min_max(double numb, double min, double max)
 	return (numb);
 }
 
-/*double	calc_spec(t_env *env, t_obj *obj)
-{
-	float	spec;
-	float	tmp;
-	t_vec	ref;
-
-	spec = 0.0;
-	ref = scale_vect(&mlx->norm, (2.0 * ft_vectordot(env->norm, dist)));
-	ref = sub_vect(dist, ref);
-	if ((tmp = dot_vect(ref, env->ray_dir)) > 0.0)
-	{
-		spec = pow(tmp, 30.0) * 1.0;
-		spec = min_max(spec, 0.0, 1.0);
-	}
-	return (spec);
-}
-*/
 double   calc_lamb(t_env *env, t_obj *obj)
 {
 	t_vec   dist;
 	t_vec   cam;
 	t_vec   norm;
 	double  lambert;
+	double	tmp;
+	double	spec;
+	t_vec	ref;
 
 	cam = add_vect(env->ray_pos, scale_vect(env->ray_dir, env->d));
 	norm = normale(obj ,env ,cam);
 	dist = sub_vect(env->spots->pos, cam);
 	dist = norm_vect(dist);
-	lambert = 0;
-	lambert += dot_vect(dist, norm) * 0.9;
+	lambert = 0.0;
+	spec = 0.0;
+	ref = scale_vect(norm, (2.0 * dot_vect(norm, dist)));
+	ref = sub_vect(dist, ref);
+	if ((tmp = dot_vect(ref, env->ray_dir)) > 0.0)
+		spec = pow(tmp, 30.0);
+	lambert += dot_vect(dist, norm) * 0.9 + spec;
+	//if (shadow())
+	//	lambert -= 0.1;
 	lambert = min_max(lambert, 0.15, 1.0) ;
 	return (lambert);
 }
